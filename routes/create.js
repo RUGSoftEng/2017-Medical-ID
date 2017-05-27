963776e40cd2a52fbb3279ccc8dc147b24715589
Matt Watkins <matthewwatkins@mac.com>
2017-05-27 12:42:11 +0200

    Set mongoose promise library to bluebird; modified guestcreate's messages to match create; removed console.log when saving fields

--- routes/create.js
+++ routes/create.js
@@ -40,15 +40,4 @@ function insertHyphen(string) {
 	}
 }
 
-/*
-router.get('/card', function(req, res){
-	res.locals.card = true;
-	res.render('create/card');
-});
-
-router.get('/document', function(req, res){
-	res.locals.doc = true;
-	res.render('create/document');
-});*/
-
 module.exports = router;
