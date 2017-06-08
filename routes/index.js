469f27f6b782133563938b5ba3311e6c4e92c337
Sofie Lovdal <s.s.lovdal@rug.nl>
2017-06-08 14:59:02 +0200

    creating a footer, three quarters done!

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,20 @@ router.get('/', function(req, res){
 	res.render('index');
 });
 
+router.get('/about', function (req, res){
+	res.render('about');
+});
+
+router.get('/FAQ', function (req, res){
+	res.render('FAQ');
+});
+
+router.get('/copyright', function (req, res){
+	res.render('copyright');
+});
+
+router.get('/contact', function (req, res){
+	res.render('contact');
+});
+
 module.exports = router;
