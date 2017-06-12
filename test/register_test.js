42a5c87af8dfc263041d6a2d43e6d90759e0ee65
Matt Watkins <matthewwatkins@mac.com>
2017-06-12 13:46:47 +0200

    Removed logging message

--- test/register_test.js
+++ test/register_test.js
@@ -5,7 +5,6 @@ var app = require('../app').app;
 
 function testRegister(name, email, pass1, pass2, callback) {
     app.testCallback = callback;
-    console.log('HERE');
     request({
         url: "http://localhost:" + app.get('port') + "/register",
         method: "POST",
